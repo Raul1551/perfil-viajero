b182e9fffab0785a63c289c9133b2f4167d3cd1a
raul <raulfernandez484@gmail.com>
2022-10-10 17:36:21 +0200

    prueba1

---
+++ app-form.js
@@ -0,0 +1,61 @@
+// Función que captura datos desde un cuadro de texto de formulario
+function catching() {
+    const datos = {
+        name: document.getElementById("get-name").value,
+        surname: document.getElementById("get-surname").value,
+        email: document.getElementById("get-mail").value,
+        phone: document.getElementById("get-phone").value,
+        isChecked: document.getElementById('consent').checked
+    }
+
+    var json = JSON.stringify(datos)
+
+    if (datos.name == "") {
+        alert("El nombre es obligatorio");
+        document.getElementById("get-name").focus();
+    } else {
+        if (datos.surname == "") {
+            alert("El apellido es obligatorio");
+            document.getElementById("get-surname").focus();
+        } else {
+            if (datos.email == "") {
+                alert("El correo electrónico es obligatorio");
+                document.getElementById("get-mail").focus();
+            } else {
+                if (datos.phone == "") {
+                    alert("El número de teléfono es obligatorio");
+                    document.getElementById("get-phone").focus();
+                } else {
+                    if (datos.isChecked == false) {
+                        alert('Debes marcar la casilla de consentimiento');
+                    } else {
+                        console.log(json);
+                        window.location.href = "http://127.0.0.1:5500/index-pregunta1.html";
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+};
+
+document.getElementById('disc-button').addEventListener('click', ()=> {
+    catching();
+});
+
+/*document.getElementById('disc-button').addEventListener('click', function () {
+    catching();
+    var name = document.getElementById('get-name').value;
+    var surname = document.getElementById('get-surname').value;
+    var mail = document.getElementById('get-mail').value;
+    var phone = document.getElementById('get-phone').value;
+
+    var blob = new Blob([name, surname, mail, phone], {
+        type: "text/plain;charset=utf-8"
+    });
+
+    saveAs(blob, name);
+}, false);*/
---
+++ perfilViajero.js
@@ -0,0 +1,105 @@
+/*function elQueMasAparece() {
+
+    var array = ["astroturismo", "gastronomico", "astroturismo", "historico", "astroturismo", "astroturismo", "astroturismo"];
+    var array1 = ["historico", "wellnes", "astroturismo", "gastronomico", "outdoors", "historico"];
+    var array2 = ["outdoors", "astroturismo", "outdoors", "outdoors", "gastronomico", "outdoors"];
+    var array3 = ["historico", "historico", "historico", "outdoors", "outdoors", "outdoors", "outdoors", "outdoors"];
+
+
+    console.log(array, array1, array2, array3);
+
+    var arrayTotal = [...array1, ...array, ...array2, ...array3];
+
+
+    var arrayOrder = arrayTotal.sort();
+    console.log(arrayOrder);
+
+    let mapeo = {};
+    let mas_frecuente = "";
+    let valor_mayor = 0;
+
+    if (typeof arrayOrder === "string") {
+        arrayOrder = arrayOrder.split(" ");
+    }
+
+    for (let elemento of arrayOrder) {
+        if (mapeo[elemento]) {
+            mapeo[elemento]++;
+        } else {
+            mapeo[elemento] = 1;
+        }
+
+    };
+
+    for (let elemento in mapeo) {
+        if (mapeo[elemento] > valor_mayor) {
+            valor_mayor = mapeo[elemento];
+            mas_frecuente = elemento;
+        }
+    }
+
+    
+    return {
+        "mas_frecuente": mas_frecuente,
+        "valor_mayor": valor_mayor
+    };
+
+    
+    
+};
+
+console.log(elQueMasAparece());
+*/
+
+var resultado4 = JSON.parse(sessionStorage.getItem("pregunta4"));
+var resultado7 = JSON.parse(sessionStorage.getItem("pregunta7"));
+var resultado8 = JSON.parse(sessionStorage.getItem("pregunta8"));
+var resultado10 = JSON.parse(sessionStorage.getItem("pregunta10"));
+var resultado11 = JSON.parse(sessionStorage.getItem("pregunta11"));
+var resultado12 = JSON.parse(sessionStorage.getItem("pregunta12"));
+var resultado13 = JSON.parse(sessionStorage.getItem("pregunta13"));
+var resultado14 = JSON.parse(sessionStorage.getItem("pregunta14"));
+
+function elQueMasAparece() {
+
+    var valorDesdeLS = document.getElementById('valorDesdeLS');
+    var arrayTotal = [...resultado4, ...resultado7, ...resultado8, ...resultado10, ...resultado11, ...resultado12, ...resultado13, ...resultado14];
+    var arrayOrder = arrayTotal.sort();
+    console.log(arrayOrder);
+
+    var mapeo = {};
+    var mas_frecuente = "";
+    var valor_mayor = 0;
+
+    if (typeof arrayOrder === "string") {
+        arrayOrder = arrayOrder.split(" ");
+    }
+
+    for (let elemento of arrayOrder) {
+        if (mapeo[elemento]) {
+            mapeo[elemento]++;
+        } else {
+            mapeo[elemento] = 1;
+        }
+
+    };
+
+    for (let elemento in mapeo) {
+        if (mapeo[elemento] > valor_mayor) {
+            valor_mayor = mapeo[elemento];
+            mas_frecuente = elemento;
+        }
+    }
+
+    valorDesdeLS.innerHTML = "¡" + mas_frecuente + "!";
+
+    /*return [mas_frecuente, valor_mayor];*/
+};
+
+
+var button = document.getElementById('button-final');
+button.addEventListener('click', () => {
+    elQueMasAparece();
+});
+
+
